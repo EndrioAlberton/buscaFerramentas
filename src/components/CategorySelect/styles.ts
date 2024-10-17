fcba6a5e434d8b4e277326d7a6ce455e06d445ac
EndrioAlberton <endrio.alberton@gmail.com>
2024-10-16 22:36:14 -0300

    style: add border radius to select

--- src/components/CategorySelect/styles.ts
+++ src/components/CategorySelect/styles.ts
@@ -4,7 +4,7 @@ export const CategoriesContainer = styled.div`
   width: 100%;
   display: flex;
   justify-content: center;
-  align-items: stretch; /* Força os filhos a terem a mesma altura */
+  align-items: stretch;
 
   @media (max-width: 600px) {
     flex-direction: column;
@@ -16,6 +16,16 @@ export const Select = styled.select`
   padding: 10px;
   font-size: 16px;
   width: 200px;
+  border-radius: 15px;
+  border: 1px solid #282c34;
+  appearance: none; /* Remove o estilo padrão */
+  background: white;
+  outline: none;
+
+  &:focus {
+    border-color: #61dafb;
+    box-shadow: 0 0 5px rgba(97, 218, 251, 0.5);
+  }
 
   @media (max-width: 600px) {
     width: 90%;
